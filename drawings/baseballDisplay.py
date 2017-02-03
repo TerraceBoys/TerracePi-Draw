1b51694be9bb9920ba80c0287abb9e5973bf792e
Brian Cox <brian5021@verizon.net>
2017-02-03 00:39:50 +0000

    redesigned the strikes

--- drawings/baseballDisplay.py
+++ drawings/baseballDisplay.py
@@ -19,7 +19,6 @@ def main(matrix, game_state):
     draw = ImageDraw.Draw(image)  
     drawRedScore(str(game_state.score1))
     drawBlueScore(str(game_state.score2))
-    #drawBatting(int(game_state.batting))
     drawDiamond()
     drawBases(int(game_state.base1), int(game_state.base2), int(game_state.base3))
     drawInning(str(game_state.inning), int(game_state.batting))
@@ -58,29 +57,19 @@ def drawBlueScore(score2):
     draw.line([0, 30, 0, 17], fill=BORDER)
     draw.text([3, 18], score2 if len(score2) > 1 else "0{0}".format(score2), font=NUM_FONT, fill=SCORE_COLOR)
 
-def drawBatting(batting):
-    global draw
-    team1 = [18, 0, 18, 15]
-    team2 = [18, 16, 18, 31]
-    if not batting:
-         draw.line(team1, fill="yellow")
-         draw.line([1, 1, 16, 1], fill='white')
-    else:
-        draw.line(team2, fill="yellow")
-
 def drawDiamond():
     global draw
     BASE_PATH = "green"
-    draw.line([37, 27, 47, 17], fill=BASE_PATH) # home - 1st
+    #draw.line([37, 27, 47, 17], fill=BASE_PATH) # home - 1st
     draw.line([47, 14, 37, 4], fill=BASE_PATH) # 1st - 2nd
     draw.line([34, 4, 24, 14], fill=BASE_PATH) # 2nd - 3rd
-    draw.line([24, 17, 34, 27], fill=BASE_PATH) # 3rd - home
+    #draw.line([24, 17, 34, 27], fill=BASE_PATH) # 3rd - home
 
 def drawBases(base1, base2, base3):
     global draw
     NO_RUNNER = "grey"
     RUNNER = "yellow"
-    draw.rectangle([35, 28, 36, 29], fill=NO_RUNNER) # home
+    #draw.rectangle([35, 28, 36, 29], fill=NO_RUNNER) # home
     draw.rectangle([47, 14, 50, 17], fill=RUNNER if base1 else NO_RUNNER) # 1st
     draw.rectangle([34, 1, 37, 4], fill=RUNNER if base2 else NO_RUNNER) # 2nd
     draw.rectangle([21, 14, 24, 17], fill=RUNNER if base3 else NO_RUNNER) # 3rd       
@@ -88,29 +77,34 @@ def drawBases(base1, base2, base3):
 def drawInning(inning, batting):
     global draw
     INNING_COLOR = "white"
-    draw.line([31, 10, 31, 16], fill=INNING_COLOR)
-    draw.text([36, 7], inning, font=NUM_FONT, fill=INNING_COLOR)
+    draw.line([31, 13, 31, 19], fill=INNING_COLOR)
+    draw.text([36, 10], inning, font=NUM_FONT, fill=INNING_COLOR)
     if not batting:
-        draw.line([30, 11, 32, 11], fill=INNING_COLOR)
-        draw.line([29, 12, 33, 12], fill=INNING_COLOR)
+        draw.line([30, 14, 32, 14], fill=INNING_COLOR)
+        draw.line([29, 15, 33, 15], fill=INNING_COLOR)
     else:
-        draw.line([29, 14, 33, 14], fill="white")
-        draw.line([30, 15, 32, 15], fill="white")
+        draw.line([29, 17, 33, 17], fill=INNING_COLOR)
+        draw.line([30, 18, 32, 18], fill=INNING_COLOR)
    
 def drawStrikes(strikes):
     global draw
     FILLED = "red"
-    NOT_FILLED = "white"
-    draw.rectangle([34, 18, 37, 19], fill=FILLED if strikes > 0 else NOT_FILLED) # top
-    draw.rectangle([34, 21, 37, 22], fill=FILLED if strikes > 1 else NOT_FILLED) # middle
-    draw.rectangle([34, 24, 37, 25], fill=NOT_FILLED) # bottom
+    draw.rectangle([22, 23, 49, 30], outline="grey")  
+    if strikes > 0:
+        draw.line([25, 24, 30, 29], fill=FILLED)
+        draw.line([25, 29, 30, 24], fill=FILLED)
+    if strikes > 1:
+        draw.line([33, 24, 38, 29], fill=FILLED)
+        draw.line([33, 29, 38, 24], fill=FILLED)
+    if strikes > 2:
+        draw.line([41, 24, 46, 29], fill=FILLED)
+        draw.line([41, 29, 46, 24], fill=FILLED)
 
 def drawOuts(outs):
     global draw
     BORDER = "blue"
     NOT_OUT = "black"
     OUT = "red"
-    
     draw.rectangle([54, 2, 61, 9], fill=OUT if outs > 0 else NOT_OUT, outline=BORDER) # top
     draw.rectangle([54, 12, 61, 19], fill=OUT if outs > 1 else NOT_OUT, outline=BORDER) # middle
     draw.rectangle([54, 22, 61, 29], fill=NOT_OUT, outline=BORDER)
